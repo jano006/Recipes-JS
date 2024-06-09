8af94397dfb970fd3909e02d08c58ff85532f95f
janker6 <janker6@gmail.com>
2024-06-09 10:06:34 +0200

    ...

---
+++ src/main/java/org/example/recipesworking/RecipesWorkingApplication.java
@@ -0,0 +1,13 @@
+package org.example.recipesworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesWorkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesWorkingApplication.class, args);
+    }
+
+}
